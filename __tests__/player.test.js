b811453af0e57481560532a1ab37836c7460e18f
brndn-do <dobrandon05@gmail.com>
2025-06-22 09:31:27 -0500

    test: players have unique gameboards

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -1,4 +1,6 @@
 import Player from "../src/player.js";
+import Gameboard from "../src/gameboard.js";
+jest.mock("../src/gameboard.js");
 
 test("real player has real property true", () => {
   const real = new Player(true);
@@ -12,4 +14,12 @@ test("computer has real property false", () => {
 
 test("throws when given bad arguments", () => {
   expect(() => new Player("a")).toThrow("argument");
+})
+
+test("each has own gameboard", () => {
+  Gameboard.mockClear();
+  const real = new Player(true);
+  const computer = new Player(false);
+  expect(real.gameboard).not.toBe(computer.gameboard);
+  expect(Gameboard).toHaveBeenCalledTimes(2);
 })
