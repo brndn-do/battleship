4de780d25b8aaa6f6578d16e4aaf18455c8e0b47
brndn-do <dobrandon05@gmail.com>
2025-06-22 12:19:52 -0500

    wip: layouts for board

---
+++ src/game.js
@@ -0,0 +1,43 @@
+function game() {
+  const main = document.querySelector(".main");
+
+  const game = document.createElement("div");
+  game.classList.add("game");
+
+  const player = document.createElement("div");
+  player.classList.add("player");
+  const playerText = document.createElement("h1");
+  playerText.textContent = "player";
+  player.appendChild(playerText);
+
+  const computer = document.createElement("computer");
+  computer.classList.add("computer");
+  const computerText = document.createElement("h1");
+  computerText.textContent = "computer";
+  computer.appendChild(computerText);
+
+  const playerBoard = document.createElement("div");
+  playerBoard.classList.add("playerBoard");
+
+  const computerBoard = document.createElement("div");
+  computerBoard.classList.add("computerBoard");
+
+  for (let i = 0; i < 100; i++) {
+    const playerCell = document.createElement("div");
+    playerCell.classList.add("cell");
+    const computerCell = document.createElement("div");
+    computerCell.classList.add("cell");
+    playerBoard.appendChild(playerCell);
+    computerBoard.appendChild(computerCell);
+  }
+
+  player.appendChild(playerBoard);
+  computer.appendChild(computerBoard);
+
+  game.appendChild(player);
+  game.appendChild(computer);
+
+  main.appendChild(game);
+}
+
+export default game;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import "./styles.css";
 import "typeface-roboto"
 
 import landing from "./landing.js";
+import game from "./game.js";
 
-landing();
+//landing();
+game();
