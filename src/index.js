d3d91c3a45870de22285d89250081bb0bd1ac568
brndn-do <dobrandon05@gmail.com>
2025-06-24 10:00:07 -0500

    feat: render board

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import "./styles.css";
 import "typeface-roboto";
 
-import { landing } from "./interface.js";
+import Interface from "./interface.js";
+import Player from "./player.js";
 
-landing();
+const ui = new Interface();
+ui.landing();
+
+// create new players
+const player = new Player(true);
+const computer = new Player(false);
+
+ui.clearPage();
+ui.game();
+
+// place ships
+player.gameboard.place(1, 3, 2);
+player.gameboard.place(2, 6, 3);
+player.gameboard.place(3, 1, 3, true);
+
+computer.gameboard.place(1, 3, 2);
+computer.gameboard.place(2, 6, 3);
+computer.gameboard.place(3, 1, 3, true);
+
+ui.renderBoard(player);
+ui.renderBoard(computer);
+
+// start game
+
+
+
--- src/interface.js
+++ src/interface.js
@@ -1,71 +1,97 @@
-function clearPage() {
-  const main = document.querySelector(".main");
-  const component = document.querySelector(".main *");
-  main.removeChild(component);
-}
+class Interface {
+  constructor() {}
 
-function landing() {
-  const main = document.querySelector(".main");
-  const landing = document.createElement("div");
-  landing.classList.add("landing");
-
-  const heading = document.createElement("h1");
-  heading.classList.add("heading");
-  heading.textContent = "battleship";
-
-  const button = document.createElement("button");
-  button.textContent = "play";
-  button.addEventListener("click", () => {
-    clearPage();
-    game();
-  })
-
-  landing.appendChild(heading);
-  landing.appendChild(button);
-
-  main.appendChild(landing);
-}
-
-function game() {
-  const main = document.querySelector(".main");
-
-  const game = document.createElement("div");
-  game.classList.add("game");
-
-  const player = document.createElement("div");
-  player.classList.add("player");
-  const playerText = document.createElement("h1");
-  playerText.textContent = "player";
-  player.appendChild(playerText);
-
-  const computer = document.createElement("computer");
-  computer.classList.add("computer");
-  const computerText = document.createElement("h1");
-  computerText.textContent = "computer";
-  computer.appendChild(computerText);
-
-  const playerBoard = document.createElement("div");
-  playerBoard.classList.add("board");
-
-  const computerBoard = document.createElement("div");
-  computerBoard.classList.add("board");
-
-  for (let i = 0; i < 100; i++) {
-    const playerCell = document.createElement("div");
-    playerCell.classList.add("cell");
-    const computerCell = document.createElement("div");
-    computerCell.classList.add("cell");
-    playerBoard.appendChild(playerCell);
-    computerBoard.appendChild(computerCell);
+  // clears page
+  clearPage() {
+    const main = document.querySelector(".main");
+    const component = document.querySelector(".main *");
+    main.removeChild(component);
   }
 
-  player.appendChild(playerBoard);
-  computer.appendChild(computerBoard);
+  // renders landing page
+  landing() {
+    const main = document.querySelector(".main");
+    const landing = document.createElement("div");
+    landing.classList.add("landing");
 
-  game.appendChild(player);
-  game.appendChild(computer);
+    const heading = document.createElement("h1");
+    heading.classList.add("heading");
+    heading.textContent = "battleship";
 
-  main.appendChild(game);
+    const button = document.createElement("button");
+    button.textContent = "play";
+    button.addEventListener("click", () => {
+      this.clearPage();
+      this.game();
+    });
+
+    landing.appendChild(heading);
+    landing.appendChild(button);
+
+    main.appendChild(landing);
+  }
+
+  // renders game page
+  game() {
+    const main = document.querySelector(".main");
+
+    const game = document.createElement("div");
+    game.classList.add("game");
+
+    const player = document.createElement("div");
+    player.classList.add("player");
+    const playerText = document.createElement("h1");
+    playerText.textContent = "player";
+    player.appendChild(playerText);
+
+    const computer = document.createElement("div");
+    computer.classList.add("computer");
+    const computerText = document.createElement("h1");
+    computerText.textContent = "computer";
+    computer.appendChild(computerText);
+
+    const playerBoard = document.createElement("div");
+    playerBoard.classList.add("board");
+
+    const computerBoard = document.createElement("div");
+    computerBoard.classList.add("board");
+
+    for (let i = 0; i < 100; i++) {
+      const playerCell = document.createElement("div");
+      playerCell.classList.add("player");
+      playerCell.classList.add("cell");
+      playerCell.classList.add(`cell${i}`);
+      const computerCell = document.createElement("div");
+      computerCell.classList.add("computer");
+      computerCell.classList.add("cell");
+      computerCell.classList.add(`cell${i}`);
+      playerBoard.appendChild(playerCell);
+      computerBoard.appendChild(computerCell);
+    }
+
+    player.appendChild(playerBoard);
+    computer.appendChild(computerBoard);
+
+    game.appendChild(player);
+    game.appendChild(computer);
+
+    main.appendChild(game);
+  }
+
+  // given a player object, renders their board
+  renderBoard(player) {
+    const grid = player.gameboard.grid;
+    for (let r = 0; r < 10; r++) {
+      for (let c = 0; c < 10; c++) {
+        if (grid[r][c].ship) {
+          const cell = document.querySelector(
+            `.${player.real ? "player" : "computer"}.cell${r * 10 + c}`
+          );
+          cell.classList.add("occupied");
+        }
+      }
+    }
+  }
 }
 
-export { landing };
+export default Interface;
